71c423123e753363866847a2b2a3ab7cdd71749b
Nabin Khatri <submergednabin@gmail.com>
2021-05-14 12:38:32 +1000

    first commit

---
+++ netflix-clone/src/Banner.js
@@ -0,0 +1,37 @@
+import React, { useState, useEffect } from "react";
+import axios from "./axios";
+import requests from "./requests";
+
+function Banner() {
+  const [movie, setMovie] = useState([]);
+  useEffect(() => {
+    async function fetchData() {
+      const request = await axios.get(requests.fetchNetflixOriginals);
+
+      setMovie(
+        request.data.results[Math.floor(Math.random() * requests.length - 1)]
+      );
+      return request;
+    }
+    fetchData();
+  }, []);
+  console.log(movie);
+  return (
+    <header
+      className="banner"
+      style={{
+        backgroundSize: "cover",
+        backgroundImage: `url("https://image.tmdb.org/t/p/original/${movie?.backdrop_path}")`,
+        backgroundPosition: "center center",
+      }}
+    >
+      <div className="banner__contents">
+        {/** Title */}
+        <h1>{movie?.title || movie?.name || movie?.original_name}</h1>
+        {/** div > 2 buttons*/}
+      </div>
+    </header>
+  );
+}
+
+export default Banner;
---
+++ netflix-clone/src/requests.js
@@ -0,0 +1,14 @@
+const API_KEY = "c911fa67751c0e86c90218fb20e28482";
+
+const requests = {
+  fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
+  fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
+  fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
+  fetchActionMovies: `discover/movie?api_key=${API_KEY}&with_genres=28`,
+  fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
+  fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
+  fetchRomanceMovies: `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
+  fetchDocumentaries: `/discover/movie?api_key=${API_KEY}&with_genres=99`,
+};
+
+export default requests;
